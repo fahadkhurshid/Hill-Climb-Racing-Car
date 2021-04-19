1e3b1018eb9ce1b499a545c773183ecbb54125b0
DESKTOP-6G7QCMK\HP <azharalia816@gmail.com>
2021-04-19 11:35:33 +0500

    i have uploaded some script and sprites

---
+++ lab assgmnt#2 azhar ali/gameentry.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gameentry : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void start_game()
+    {
+        SceneManager.LoadScene("gamescene");
+    }
+    public void exitgame()
+    {
+        Debug.Log("exitgame");
+        Application.Quit();
+    }
+}
